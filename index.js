04cebae9f013fc46a623ad2a7bdc4efc2655c10f
Wayne Otido <otidowayne@gmail.com>
2023-06-14 14:01:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } 
+  if(feet > 2500) {
+    return 'No can do.'
+  }
+  if (feet >= 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city != 'NYC' ? 'No go.' : 'Ok, sounds good.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  tip; 
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
